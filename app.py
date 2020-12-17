465eb9f3452f2d9e6e3b2ea094b1f5a460c6e0ac
Vlad Korneev <VeenrokDalv@protonmail.com>
2020-12-17 22:51:49 +0300

    The class of the appendix and its methods is created

---
+++ app.py
@@ -0,0 +1,42 @@
+import sys
+import pygame as pg
+
+
+class App:
+
+    def __init__(self, width: int = 640, height: int = 640, pixel: int = 10):
+        """
+            Initialization main settings app.
+            :param width: Width window.
+            :param height: Height window.
+            :param pixel: size virtual pixel window.
+            """
+        pass
+
+    def close(self):
+        """
+        Finishes all processes of the appendix
+        :return: None
+        """
+        pass
+
+    def event_handler(self):
+        """
+        Catches all events of the appendix
+        :return:
+        """
+        pass
+
+    def blit(self):
+        """
+        Process of display of all objects in a window
+        :return: None
+        """
+        pass
+
+    def start(self):
+        """
+        Start of all processes of the appendix
+        :return: None
+        """
+        pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from app import App
+
+def main():
+    game = App()
+    game.start()
+
+if __name__ == '__main__':
+    main()
