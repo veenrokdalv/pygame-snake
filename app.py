3a835054a818a1c1826613345a519d0e49c6df90
Vlad Korneev <VeenrokDalv@protonmail.com>
2020-12-17 23:29:09 +0300

    Revert "The class of the appendix and its methods is created"
    This reverts commit f86fdf7cbaf3ed0d56d2828681bd38d2224ed1ab.

--- app.py
+++ app.py
@@ -11,66 +11,23 @@ class App:
             :param height: Height window.
             :param pixel: size virtual pixel window.
             """
-        # Load image icon app
-        self.icon = pg.image.load('./images/Icon.png')
-
-        # Load max score
-        with open('./maxscore.txt') as file:
-            self.max_score = int(file.read())
-
-        # Settings window
-        self.background_color = (20, 20, 20)
-
-        # WARMING! : Game speed depends on fps
-        self.framerate = pg.time.Clock().tick
-        self.fps = 5
-
-        # Create window
-        self.window = pg.display.set_mode((width, height))
-        pg.display.set_caption('Snake')
-        pg.display.set_icon(self.icon)
+        pass
 
     def close(self):
         """
         Finishes all processes of the appendix
         :return: None
         """
-        pg.quit()
-        sys.exit()
+        pass
 
     def event_handler(self):
         """
         Catches all events of the appendix
         :return:
         """
-        # Events from the keyboard
-        events_keyboard = pg.key.get_pressed()
-
-        # Events which close the appendix
-        [self.close() for event in pg.event.get() if event.type == pg.QUIT]
-        if events_keyboard[pg.K_ESCAPE]:
-            self.close()
-
-        # Game management
-        if events_keyboard[pg.K_UP]:
-            print('Изменено направление змейки K_UP')
-        if events_keyboard[pg.K_DOWN]:
-            print('Изменено направление змейки K_DOWN')
-        if events_keyboard[pg.K_LEFT]:
-            print('Изменено направление змейки K_LEFT')
-        if events_keyboard[pg.K_RIGHT]:
-            print('Изменено направление змейки  K_RIGHT')
-
-
-
-    def move_objects(self):
-        """
-        Process which sets the provision of objects at a window
-        :return: None
-        """
         pass
 
-    def blit_objects(self):
+    def blit(self):
         """
         Process of display of all objects in a window
         :return: None
@@ -82,9 +39,4 @@ class App:
         Start of all processes of the appendix
         :return: None
         """
-        while True:
-            self.event_handler()
-            self.move_objects()
-            self.blit_objects()
-            self.framerate(self.fps)
-
+        pass
